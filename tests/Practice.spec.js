11cd48bad83170134c3b16209233addd8d2549fb
Andyadav1 <anandkhushi309@gmail.com>
2026-03-18 12:02:22 +0530

    comment changes

--- tests/Practice.spec.js
+++ tests/Practice.spec.js
@@ -5,13 +5,13 @@ test("Add/Remove_Elements", async ({ page }) => {
   await page.goto("https://the-internet.herokuapp.com/add_remove_elements/");
   let addElement = page.locator("//button[text()='Add Element']");
   let elements = page.locator("//button[text()='Delete']");
-  for (; (await elements.count()) < 5; ) {
+  for (;(await elements.count()) < 5; ) {
     await addElement.click();
   }
   expect(await elements.count()).toEqual(5);
 
   //await addElement.click();
-  //while loop is also possible but not recommended as its infinite and may break or memory  issues
+  //while loop is also possible but not recommended as its infinite and may break or cause memory  issues
   // while (await elements.first().isVisible()) {
   //   await addElement.click();
   //   if ((await elements.count()) === 5) {
@@ -35,9 +35,9 @@ test(`Basic_Auth`, async ({ page }) => {
 test(`Challenging_DOM`, async ({ page }) => {
   await page.goto("https://the-internet.herokuapp.com/challenging_dom");
   //button locators
-  let blue_btn = await page.locator('//a[@class="button"]');
-  let red_btn = await page.locator('//a[@class="button alert"]');
-  let green_btn = await page.locator('//a[@class="button success"]');
+  let blue_btn = page.locator('//a[@class="button"]');
+  let red_btn = page.locator('//a[@class="button alert"]');
+  let green_btn = page.locator('//a[@class="button success"]');
 
   //table locators
   let table;
